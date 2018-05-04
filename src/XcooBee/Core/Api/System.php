31d37a806af1c9c26f541fff2ebede7fcdc0a738
Ganesh <ourdailywork@gmail.com>
2018-05-04 13:59:29 +0530

    format

--- src/XcooBee/Core/Api/System.php
+++ src/XcooBee/Core/Api/System.php
@@ -20,29 +20,29 @@ class System extends Api
 		$this->_consent = new Consents();
 
 	}
+	
 	/**
      * method to check if pgp key and Campaign is correct.
      *
      * @return \XcooBee\Http\Response
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-
 	public function ping()
 	{
 		$user = $this->_users->getUser();
 		$response = new Response();
 		if ($user->pgp_public_key) {
-            $campaignInfo = $this->_consent->getCampaignInfo();
-            if (!empty($campaignInfo->data->campaign)) {
-                $response->code = 200;
-            } else {
-                $response->code = 400;
-                $response->errors = "campaign not found.";
-            }
-        } else {
-            $response->code = 400;
-            $response->errors = "pgp key not found.";
-        }
+			$campaignInfo = $this->_consent->getCampaignInfo();
+			if (!empty($campaignInfo->data->campaign)) {
+				$response->code = 200;
+			} else {
+				$response->code = 400;
+				$response->errors = "campaign not found.";
+			}
+		} else {
+			$response->code = 400;
+			$response->errors = "pgp key not found.";
+		}
 
 		return $response;	
 	}
