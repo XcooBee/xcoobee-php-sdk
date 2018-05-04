a617e11bf123770f3f887e29e020951acec6eb06
Ganesh <ourdailywork@gmail.com>
2018-05-04 11:54:06 +0530

    Test Cases
    Unit testing for ping method

---
+++ test/src/XcooBee/Core/Api/SystemTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Test\XcooBee\Core\Api;
+
+use XcooBee\Test\TestCase;
+
+class SystemTest extends TestCase
+{
+    public function testPing()
+    {
+        $systemMock = $this->_getMock(\XcooBee\Core\Api\System::class, [
+            '_getDefaultCampaignId' => null,
+        ]);
+        $this->_setProperty($systemMock, '_users', $this->_getMock(Users::class, [
+            'getUser' => (object) ['pgp_public_key' => 'test']
+        ]));
+        $this->_setProperty($systemMock, '_consent', $this->_getMock(Users::class, [
+            'getCampaignInfo' => (object) [
+            'data' => (object) [
+            'campaign' => (object) [
+            'xcoobee_targets' => [],
+        ],
+        ],
+        ]
+        ]));
+
+        $response = $systemMock->ping();
+		$this->assertEquals(200, $response->code);
+    }
+    
+    public function testPing_NoCampaignProvided()
+    {
+        $systemMock = $this->_getMock(\XcooBee\Core\Api\System::class, [
+            '_getDefaultCampaignId' => null,
+        ]);
+        $this->_setProperty($systemMock, '_users', $this->_getMock(Users::class, [
+            'getUser' => (object) ['pgp_public_key' => 'test']
+        ]));
+        $this->_setProperty($systemMock, '_consent', $this->_getMock(Users::class, [
+            'getCampaignInfo' => (object) [
+            'data' => null
+        ]
+        ]));
+		
+       $response = $systemMock->ping();
+       $this->assertEquals(400, $response->code);
+    }
+    
+    public function testPing_NoPGP()
+    {
+        $systemMock = $this->_getMock(\XcooBee\Core\Api\System::class, [
+            '_getDefaultCampaignId' => null,
+        ]);
+        $this->_setProperty($systemMock, '_users', $this->_getMock(Users::class, [
+            'getUser' => (object) ['pgp_public_key' => null]
+        ]));
+
+        $response = $systemMock->ping();
+		$this->assertEquals(400, $response->code);
+    }
+}
