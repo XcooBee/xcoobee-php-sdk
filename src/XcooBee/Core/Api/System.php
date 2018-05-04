d1ddecea9c167ea31d375f00000fdd140a5adf51
Ganesh <ourdailywork@gmail.com>
2018-05-04 13:12:36 +0530

    formatting
    PSR-2 formatting

--- src/XcooBee/Core/Api/System.php
+++ src/XcooBee/Core/Api/System.php
@@ -8,31 +8,31 @@ use XcooBee\Core\Configuration;
 class System extends Api
 {
 	/** @var Users */
-    protected $_users;
-    /** @var Consent */
-    protected $_consent;
-    /** @var Response */
-    protected $_response;
-    
+	protected $_users;
+	/** @var Consent */
+	protected $_consent;
+	/** @var Response */
+	protected $_response;
+
 	public function __construct()
-    {
+	{
 		parent::__construct();
-		
-        $this->_users = new Users();
-        $this->_consent = new Consents();
-        
-    }
-    /**
+
+		$this->_users = new Users();
+		$this->_consent = new Consents();
+
+	}
+	/**
      * method to check if pgp key and Campaign is correct.
      *
      * @return \XcooBee\Http\Response
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-     
-    public function ping()
-    {
-        $user = $this->_users->getUser();
-        $this->_response = new Response();
+
+	public function ping()
+	{
+		$user = $this->_users->getUser();
+		$this->_response = new Response();
 		if($user->pgp_public_key){
 			$campaignInfo=$this->_consent->getCampaignInfo();
 			if(!empty($campaignInfo->data->campaign))
@@ -47,7 +47,7 @@ class System extends Api
 			$this->_response->code=400;
 			$this->_response->errors="pgp key not found.";	
 		}
-		
+
 		return $this->_response;	
-    }
+	}
 }
