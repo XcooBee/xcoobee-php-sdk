0466318b9b106704a7ede6689ffda72d99d82c7b
Ganesh <ourdailywork@gmail.com>
2018-05-09 17:41:54 +0530

    Fect User Conversations
    Methods to get conversations
    getConversations()
    getConversation()

--- src/XcooBee/Core/Api/Users.php
+++ src/XcooBee/Core/Api/Users.php
@@ -38,4 +38,63 @@ class Users extends Api
 
         return $user;
     }
+    
+    /**
+     * list all the user conversation
+     *
+     * @param array $data
+     * 
+     * @return \XcooBee\Http\Response
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getConversations($data = array(), $records = null) {
+        $query = 'query getConversations($userId: String!,$first : Int) {
+            conversations(user_cursor: $userId, first : $first) {
+                data {
+                    display_name,
+                    consent_cursor,
+                    target_cursor,
+                    date_c,
+                }
+                page_info {
+                    end_cursor
+                    has_next_page
+                }
+            }
+        }';
+
+        return $this->_request($query, array_merge(['first' => $records,'userId' => $this->_getUserId()], $data));
+    }
+
+    /**
+     * get conversation data
+     *
+     * @param string $conversationID
+     * @param Int $records
+     * 
+     * @return \XcooBee\Http\Response
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getConversation($conversationID, $records = null) {
+        if (!$conversationID) {
+            throw new XcooBeeException('No "conversation" provided');
+        }
+
+        $query = 'query getConversation($conversationID: String!,$first : Int) {
+		conversation(target_cursor: $conversationID, first : $first) {
+                    data {
+                        display_name,
+                        consent_cursor,
+                        target_cursor,
+                        date_c,
+                    }
+                    page_info {
+                        end_cursor
+                        has_next_page
+                    }
+		}
+	}';
+
+        return $this->_request($query, ['first' => $records, 'conversationID' => $conversationID]);
+    }
 }
