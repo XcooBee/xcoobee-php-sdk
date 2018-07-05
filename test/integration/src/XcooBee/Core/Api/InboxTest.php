8d12dc1ed3bb897df9d0cbc38dfb5b82d25466ca
Ganesh <ourdailywork@gmail.com>
2018-07-05 19:32:11 +0530

    system API integration tests

--- test/integration/src/XcooBee/Core/Api/InboxTest.php
+++ test/integration/src/XcooBee/Core/Api/InboxTest.php
@@ -2,19 +2,22 @@
 
 namespace Test\XcooBee\Core\Api;
 
-use XcooBee\Test\TestCase;
+use XcooBee\Test\IntegrationTestCase;
 
-class InboxTest extends TestCase 
+class InboxTest extends IntegrationTestCase 
 {
 
     public function testListInbox()
     {
-        
+        $inboxData = $this->_xcoobee->inbox->listInbox();
+        print_r($inboxData);
+        //$this->assertEquals(200, $inboxData->code);
     }
 
     public function testGetInboxItem()
     {
-        
+        $response = $this->_xcoobee->inbox->getInboxItem("mytestmessge.jpg.eee03714-11c6-4134-af22-0d71338d12ce");
+        print_r($response);
     }
 
     public function testDeleteInboxItem()
--- test/integration/src/XcooBee/Core/Api/SystemTest.php
+++ test/integration/src/XcooBee/Core/Api/SystemTest.php
@@ -2,33 +2,46 @@
 
 namespace Test\XcooBee\Core\Api;
 
-use XcooBee\Test\TestCase;
+use XcooBee\Test\IntegrationTestCase;
 
-class SystemTest extends TestCase
+class SystemTest extends IntegrationTestCase
 {
     public function testPing()
     {
-        
+        $response = $this->_xcoobee->system->ping();
+        $this->assertEquals(200, $response->code);
+        $this->assertEquals(null, $response->data);
     }
     
-    public function testListEventSubscriptions() 
+    public function testListEventSubscriptions()
     {
-        
+        $response = $this->_xcoobee->system->listEventSubscriptions();
+        $this->assertEquals(200, $response->code);
+        $this->assertEquals((object)[
+            'event_type' => 'consent_approved',
+            'handler' => 'testHandler',
+            'date_c' => '2018-05-23T08:02:25Z'
+        ], $response->data->event_subscriptions->data[0]);
     }
 
-    public function testAddEventSubscription() 
+    public function testAddEventSubscription()
     {
-        
+        $response = $this->_xcoobee->system->addEventSubscription(['UserDataRequest' => 'testEventHandler']);
+        $this->assertEquals(200, $response->code);
+        $this->assertEquals((object)['event_type' => 'user_data_request'], $response->data->add_event_subscriptions->data[0]);
     }
 
-    public function testDeleteEventSubscription() 
+    public function testDeleteEventSubscription()
     {
-        
+        $response = $this->_xcoobee->system->deleteEventSubscription(['UserDataRequest']);
+        $this->assertEquals(200, $response->code);
+        $this->assertEquals((object)['deleted_number' => 1], $response->data->delete_event_subscriptions);
     }
     
     public function testGetEvents()
     {
-       
+       $response = $this->_xcoobee->system->getEvents();
+       print_r($response);
     }
     
 }
