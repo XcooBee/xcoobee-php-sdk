3236a2795fe21862193779abe90dcbebca0a846e
Mahmoud Saeed <msaeed.ma@gmail.com>
2018-12-02 00:49:24 +0200

    Add example for  addEventSubscription()

---
+++ example/add_event_subscription.php
@@ -0,0 +1,25 @@
+<?php
+require '../vendor/autoload.php';
+
+use XcooBee\XcooBee;
+
+$xcoobee = new XcooBee();
+
+// Set configuration from the config file located at your home directory.
+//  e.g. /home/user/.xcoobee/config on POSIX-compliant systems,
+//  or /Users/MyUserDir/.xcoobee/config on Windows
+$xcoobee->setConfig(\XcooBee\Models\ConfigModel::createFromFile());
+
+// Or, provide configuration data directly.
+// $xcoobee->setConfig(\XcooBee\Models\ConfigModel::createFromData([
+//     'apiKey'    => '',
+//     'apiSecret' => '',
+// ]));
+
+//
+$res = $xcoobee->system->addEventSubscription(
+    ['EventType', 'eventTypeHandler'],
+    'campaignId'
+);
+
+var_export($res);
